5723418c04ac9446be94405b9cf48117c9dec315
MHefny <mohammad.hefny@gmail.com>
2025-01-12 09:44:03 +0200

    fix install

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='de_sound_module',
-    version='1.0.0',
+    version='2.0.0',
+    author='Mohammad Hefny',
+    author_email='mohammad.hefny@gmail.com',
     packages=find_packages(),
     install_requires=[
         'colorama',
+        'pyttsx3',  # Install pyttsx3 for broader compatibility
     ],
+    extras_require={
+        'rpi': ['espeak-ng'],  # Optional dependency for RPi
+        'ubuntu': ['espeak-ng'],  # Optional dependency for Ubuntu
+    },
     # Other setup arguments, such as author, description, etc.
 )
